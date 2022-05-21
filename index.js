05b01fc991f02ca1ca48c123a9dfb52a1e6e5001
sujon-chambugong <sujonchambugong@gmail.com>
2022-05-21 21:21:40 +0600

    node_modules be gone!

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
+
 
 app.get('/', (req, res)=>{
  res.send('Server connected with client');
