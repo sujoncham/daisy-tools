ee5f15a0e50f1761fabdaa1a43fcc55914e3d008
sujon-chambugong <sujonchambugong@gmail.com>
2022-05-24 02:21:52 +0600

    review post and review get data api done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,7 +11,7 @@ app.use(express.json());
 // https://www.aloktools.com/
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tzls3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -18,6 +19,8 @@ async function run(){
     try{
         await client.connect();
         const productCollection = client.db('daisyTools').collection('product');
+        const reviewCollection = client.db('daisyTools').collection('reviews');
+        const purchaseCollection = client.db('daisyTools').collection('purchase');
 
         app.get('/products', async (req, res)=>{
             const products = await productCollection.find().toArray();
@@ -29,6 +32,24 @@ async function run(){
             const result = await productCollection.insertOne(product);
             res.send(result);
           });
+
+        app.get('/reviews', async (req, res)=>{
+            const reviews = await reviewCollection.find().toArray();
+            res.send(reviews);
+        })
+
+        app.post('/reviews', async (req, res)=>{ 
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        });
+
+        app.post('/purchase', async (req, res)=>{
+            const purchase = req.body;
+            const result = await purchaseCollection.insertOne(purchase);
+            res.send(result);
+        })
+
 
 
 
