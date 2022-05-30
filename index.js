07a43834551ed1ddba0255af6072fd0f13cee20d
sujon-chambugong <sujonchambugong@gmail.com>
2022-05-30 17:08:06 +0600

    myProfile api updated

--- index.js
+++ index.js
@@ -139,12 +139,12 @@ async function run(){
             res.send(result);
           });
 
-        app.get('/myProfile', async (req, res)=>{
+        app.get('/myProfile/:id', async (req, res)=>{
             const profile = await profileCollection.find().toArray();
             res.send(profile);
         })
 
-        app.post('/myProfile', async (req, res)=>{ 
+        app.post('/myProfile/:id', async (req, res)=>{ 
             const profile = req.body;
             const result = await profileCollection.insertOne(profile);
             res.send(result);
